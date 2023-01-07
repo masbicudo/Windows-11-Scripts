3908d9415473752c11f41007ab3a44ce2ee1cd00
Miguel Angelo <masbicudo@gmail.com>
2023-01-07 17:44:35 -0300

    Basic install improvements

--- basic-install.ps1
+++ basic-install.ps1
@@ -21,8 +21,8 @@
 # Input parameters
 [OutputType($null)]
 Param(
-    # [Parameter()]
-    # [switch] $Revert
+    [Parameter()]
+    [switch] $Force
 )
 
 
@@ -30,21 +30,42 @@ Param(
 $ErrorActionPreference = 'Stop'
 $InformationPreference = 'Continue'
 
+function Ask
+{
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]$Text,
+        [string]$Default
+    )
+    
+    [string]$message = "$Text [Y/N]"
+    if (!([string]::IsNullOrEmpty($Default)))
+    {
+        $message += " (default=$Default)"
+    }
 
-# # Self-elevate the script if required
-# # ref: https://blog.expta.com/2017/03/how-to-self-elevate-powershell-script.html
-# if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
-#     if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
-#         $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
-#         Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
-#         Exit
-#     }
-# }
-
-# Assets
-$Some = [string] ''
-
-function Set-Env-Variable {
+    if ($Force) {
+        Write-Information "${message}: Y"
+        return $true
+    }
+    while ($true) {
+        $value = Read-Host "$message"
+        if ([string]::IsNullOrWhiteSpace($value))
+        {
+            $value = $Default
+        }
+        if ($value -eq "Y" -Or $value -eq "y")
+        {
+            return $true
+        }
+        if ($value -eq "N" -Or $value -eq "n")
+        {
+            return $false
+        }
+    }
+}
+function Set-DotEnv-Variable {
 
     [CmdletBinding()]
     param (
@@ -73,10 +94,13 @@ function Set-Env-Variable {
     Write-Output $value
 }
 
+
 if (!(Test-Path -PathType Leaf ".env"))
 {
     Write-Information -MessageData '.env file not found'
-    Write-Information -MessageData 'Create it manually == or =='
+    Write-Information -MessageData 'You can answer some questions now to create the file'
+    Write-Information -MessageData '    == or =='
+    Write-Information -MessageData 'You can create the file yourself externally'
     Read-Host "Press Enter to continue"
 }
 
@@ -84,60 +108,115 @@ if (!(Test-Path -PathType Leaf ".env"))
 {
     Write-Information -MessageData 'Some information is needed to use scripts:'
 
-    $email = Set-Env-Variable -Variable "EMAIL" -Text "E-mail"
+    Write-Information -MessageData 'General questions about the user'
 
-    Set-Env-Variable -Variable "DNS_HE_PASSWORD" -Text "DNS HE Password"
+    $default_email = Set-DotEnv-Variable -Variable "EMAIL" -Text "E-mail"
+    $default_user = Set-DotEnv-Variable -Variable "USER_NAME" -Text "Your name"
 
-    Set-Env-Variable -Variable "CF_TOKEN" -Text "CloudFlare Account Token"
-    Set-Env-Variable -Variable "CF_DOMAIN" -Text "CloudFlare Account Domain"
-    Set-Env-Variable -Variable "CF_RECORD" -Text "CloudFlare Account Record"
-    Set-Env-Variable -Variable "CF_EMAIL" -Text "CloudFlare Account E-mail" -Default $email
+    Write-Information -MessageData 'Service provider questions'
+
+    Set-DotEnv-Variable -Variable "DNS_HE_PASSWORD" -Text "DNS HE Password"
+
+    Set-DotEnv-Variable -Variable "CF_TOKEN" -Text "CloudFlare Account Token"
+    Set-DotEnv-Variable -Variable "CF_DOMAIN" -Text "CloudFlare Account Domain"
+    Set-DotEnv-Variable -Variable "CF_RECORD" -Text "CloudFlare Account Record"
+    Set-DotEnv-Variable -Variable "CF_EMAIL" -Text "CloudFlare Account E-mail" -Default $default_email
+
+    Set-DotEnv-Variable -Variable "GIT_EMAIL" -Text "Git Config E-mail" -Default $default_email
+    Set-DotEnv-Variable -Variable "GIT_USER_NAME" -Text "Git Config User Name" -Default $default_user
 }
 
-Install-Module -Name Set-PsEnv
-Set-PsEnv
+switch -File ".env" {
+    default {
+        $name, $value = $_.Trim() -split '=', 2
+        if ($name -and $name[0] -ne '#') { # ignore blank and comment lines.
+            Write-Information -MessageData "Env:$name = $value"
+            Set-Item "Env:$name" $value
+        }
+    }
+}
+
+
+# Set execution policy
+if (Ask -Text "Would you like to set ExecutionPolicy to Bypass for CurrentUser?" -Default "N")
+{
+    Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope CurrentUser
+}
 
 
 # Install Chocolatey
-Write-Information -MessageData 'Installing Chocolatey'
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+if (Ask -Text "Would you like to install Chocolatey?" -Default "Y")
+{
+    Write-Information -MessageData 'Installing Chocolatey'
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+}
+
 
 # Install Git
-Write-Information -MessageData 'Installing Git'
-choco install -y git
-$Env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")  
+if (Ask -Text "Would you like to install Git?" -Default "Y")
+{
+    Write-Information -MessageData 'Installing Git'
+    choco upgrade -y git --params "'/GitAndUnixToolsOnPath /WindowsTerminalProfile'" --params-global
+    $Env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")  
+}
+
+if (Ask -Text "Would you like to set global Git configurations now?" -Default "Y") {
+    Write-Information -MessageData 'Setting up Git'
+    git config --global user.name "$Env:GIT_USER_NAME"
+    git config --global user.name
+    git config --global user.email "$Env:GIT_EMAIL"
+    git config --global user.email
+}
+
 
 # Create id_rsa file to use with GitHub
-Write-Information "Creating ~/.ssh/id_rsa file"
-if (!(Test-Path -PathType Leaf "$env:USERPROFILE\.ssh\id_rsa"))
-{
-    ssh-keygen -t rsa -b 4096 -C "$env:ID_RSA_EMAIL"
-    Write-Information "Paste the following content at"
-    Write-Information "https://github.com/settings/ssh/new"
-    Write-Information ""
-    Get-Content "$env:USERPROFILE\.ssh\id_rsa.pub"
-    Write-Information ""
-    Read-Host "Press Enter to continue"
+if (Ask -Text "Would you like to create ~/.ssh/id_rsa now?" -Default "Y") {
+    Write-Information "Creating ~/.ssh/id_rsa file"
+    if (!(Test-Path -PathType Leaf "$Env:USERPROFILE\.ssh\id_rsa"))
+    {
+        ssh-keygen -t rsa -b 4096 -C "$Env:ID_RSA_EMAIL"
+    }
 }
+if (Ask -Text "Would you like to setup GitHub now?" -Default "Y") {
+    if (Test-Path -PathType Leaf "$Env:USERPROFILE\.ssh\id_rsa")
+    {
+        Write-Information "Paste the following content:"
+        Write-Information ""
+        Get-Content "$Env:USERPROFILE\.ssh\id_rsa.pub"
+        Write-Information ""
+        Write-Information "at: https://github.com/settings/ssh/new"
+        Read-Host "Press Enter to continue"
+    }
+}
+
 
 # Create Projects directory
-Write-Information -MessageData 'Creating Projects directory'
-md -Force "D:\Projects"
+if (Ask -Text "Would you like to create D:\Projects directory now?" -Default "Y") {
+    Write-Information -MessageData 'Creating Projects directory'
+    md -Force "D:\Projects"
+}
+
 
 # Cloning Windows-11-Scripts
-if (Test-Path -PathType Container "D:\Projects")
-{
-    Write-Information -MessageData 'Cloning Windows-11-Scripts'
-    pushd "D:\Projects"
-    git clone "git@github.com:masbicudo/Windows-11-Scripts.git"
-    Invoke-Item ".\Windows-11-Scripts"
-    popd
+if (Ask -Text "Would you like to clone Windows-11-Scripts now?" -Default "Y") {
+    if (Test-Path -PathType Container "D:\Projects")
+    {
+        Write-Information -MessageData 'Cloning Windows-11-Scripts'
+        pushd "D:\Projects"
+        git clone "git@github.com:masbicudo/Windows-11-Scripts.git"
+        Invoke-Item ".\Windows-11-Scripts"
+        popd
+    }
+    Write-Information -MessageData 'Copying .env file to project directory'
+    $location = (Get-Location).Path
+    if (!($location -eq "D:\Projects\Windows-11-Scripts")) {
+        if ((Test-Path -PathType Leaf ".env"))
+        {
+            cp ".env" "D:\Projects\Windows-11-Scripts\" -Force
+        }    
+    }
 }
 
-if ((Test-Path -PathType Leaf ".env"))
-{
-    cp ".env" "D:\Projects\Windows-11-Scripts\" -Force
-}
 
 # Done
 Write-Information -MessageData 'Done.'
--- readme.md
+++ readme.md
@@ -7,12 +7,17 @@ This code is provided as is.
 ## Basic Install
 
 ```powershell
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/masbicudo/Windows-11-Scripts/master/basic-install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex "$Force=$true;"+((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/masbicudo/Windows-11-Scripts/master/basic-install.ps1'))
 ```
 
-The basic installation script will install the first needed tools to make further modifications to the system.
+The basic installation script will install the first needed tools to make further modifications to the system:
 
 - Chocolatey
 - Git
+  - Install
+  - Setup User
+- SSH: create id_rsa
+- GitHub: use SSH key
 - Create Projects folder at `D:\Projects`
 - Checkout `Windows-11-Scripts` inside that folder
+- (optional) Move Desktop and Downloads folder to `D:\`
